05e9cc71a6a1fd49d456c4798e59d63c28ca93d8
Roman <dubrovskijrd@yandex.ru>
2022-02-12 14:07:21 +0300

    add global url prefix /rra

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, redirect
 from flask_admin import helpers as admin_helpers
 
 from flask_babel import Babel
@@ -8,9 +8,27 @@ from app.admin import admin
 from app.views.admin import admin_views
 
 
+class PrefixMiddleware(object):
+
+    def __init__(self, app, prefix=''):
+        self.app = app
+        self.prefix = prefix
+
+    def __call__(self, environ, start_response):
+
+        if environ['PATH_INFO'].startswith(self.prefix):
+            environ['PATH_INFO'] = environ['PATH_INFO'][len(self.prefix):]
+            environ['SCRIPT_NAME'] = self.prefix
+            return self.app(environ, start_response)
+        else:
+            start_response('404', [('Content-Type', 'text/plain')])
+            return ["This url does not belong to the app.".encode()]
+
+
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
+    app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix='/rra')
     db.init_app(app)
     babel = Babel(app, default_locale="ru")
     admin.init_app(app)
@@ -26,5 +44,10 @@ def create_app():
         )
     for view in admin_views():
         admin.add_view(view)
+
+    @app.route('/')
+    def bar():
+
+        return redirect(url_for('admin.index'))
 
     return app
