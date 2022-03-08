21c867a24f2da8599b0318b0791631a758141b77
Roman <dubrovskijrd@yandex.ru>
2022-03-08 16:09:38 +0300

    Add base url for admin index view and all other views, remove unused prefix midlleware

--- app/admin.py
+++ app/admin.py
@@ -2,4 +2,4 @@ import flask_admin
 
 
 admin = flask_admin.Admin(name="РРА", base_template='my_master.html', template_mode='bootstrap4',
-                          index_view=flask_admin.AdminIndexView(name="Главная"))
+                          index_view=flask_admin.AdminIndexView(name="Главная", url='/rra'), url='/rra')
